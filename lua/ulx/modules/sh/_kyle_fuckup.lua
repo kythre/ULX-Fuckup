2a1e308bdddf8762ce2fb3fb48272fadc108b92d
Kyle <thisisnotkylesemail@gmail.com>
2019-02-18 11:25:36 -0600

    fixed tabbing

--- lua/ulx/modules/sh/_kyle_fuckup.lua
+++ lua/ulx/modules/sh/_kyle_fuckup.lua
@@ -4,19 +4,19 @@ local CATEGORY_NAME = "_Kyle_1"
 function ulx.fuckup( calling_ply, target_plys )
 	for i=1, #target_plys do
 		local v = target_plys[ i ]
-        if not v:Alive() then
-            ULib.tsayError( calling_ply, v:Nick() .. " is dead and cannot be fucked!", true )
-        else
-            if v:InVehicle() then
-                local vehicle = v:GetParent()
-                v:ExitVehicle()
-            end
-            local ragdoll = ents.Create( "prop_ragdoll" )
-            ragdoll:Spawn()
-            v:SetParent( ragdoll )
-            v:SendLua("chat.Open(1)")
-            ragdoll:Remove()
-        end
+		if not v:Alive() then
+		    ULib.tsayError( calling_ply, v:Nick() .. " is dead and cannot be fucked!", true )
+		else
+		    if v:InVehicle() then
+			local vehicle = v:GetParent()
+			v:ExitVehicle()
+		    end
+		    local ragdoll = ents.Create( "prop_ragdoll" )
+		    ragdoll:Spawn()
+		    v:SetParent( ragdoll )
+		    v:SendLua("chat.Open(1)")
+		    ragdoll:Remove()
+		end
 	end
 end
 local fuckup = ulx.command( CATEGORY_NAME, "ulx fuckup", ulx.fuckup, "!fuckup" )
